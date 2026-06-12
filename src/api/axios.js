ad9a8da1946caab7fe0d8dab924a58c37b16b9c8
AntonNesterchuk <39776539+tpAnswer4yourself@users.noreply.github.com>
2026-06-12 22:48:39 +0300

    Add files via upload

---
+++ src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// к запросам всегда будут добавляться заголовок с токеном
+// на клиенте токен хранится в localstorage
+// promise.reject возвращает отклоненный промис, чтобы клиент обработал ее в блоке catch
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
